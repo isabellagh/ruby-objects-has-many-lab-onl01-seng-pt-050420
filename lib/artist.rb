bb917d5ed7ba6db718c106254c9f6d2656811adc
isabella g henriques <isabellagh88@gmail.com>
2020-06-01 14:52:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,10 @@ class Artist
     song.artist = self
     @@song_count += 1
   end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
   def add_song_by_name(name)
     song = Song.new(name)
