adb8e54d315c7bc9e9570041868442ed859ceda7
isabella g henriques <isabellagh88@gmail.com>
2020-06-01 14:16:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,60 +1,28 @@
-class Author 
-  
-  attr_accessor :name 
-  
-  @@post_count = 0 
+class Author
+
+  attr_accessor :name, :posts
+
+  @@post_count = 0
   
   def initialize(name)
-    @name = name 
+    @name = name
     @posts = []
-  end 
-  
+  end
+
   def add_post(post)
-    self.posts << post
+    @posts << post
     post.author = self
-    @@post_count +=1  
-  end 
-  
+    @@post_count += 1
+  end
+
   def add_post_by_title(title)
-    post = post.new(title)
-    self.posts << post
+    post = Post.new(title)
+    @posts << post
     post.author = self
-    @@post_count +=1
-  end 
-  
+    @@post_count += 1
+  end
+
   def self.post_count
     @@post_count
-  end 
-    
-  def posts 
-    @posts  
-  end 
-  
-end 
-
-
-class Post 
-  
-  attr_accessor :title, :author 
-  
-  @@all = []
-  
-  def initialize(name, author=nil)
-    @name = name
-    @author = author
-    @@all << self
   end
-
-  def author
-    if self.author
-      self.author.name
-    else
-      nil
-    end
-  end
-
-  def self.all
-    @@all
-  end
-  
-end 
+end
