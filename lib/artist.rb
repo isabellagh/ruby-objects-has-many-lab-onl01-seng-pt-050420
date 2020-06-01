f50cc8baf33718cf61b4538105ea17d63e0bc641
isabella g henriques <isabellagh88@gmail.com>
2020-06-01 13:13:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,4 +39,22 @@ class Song
   
   @@all = []
   
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+  
 end 
