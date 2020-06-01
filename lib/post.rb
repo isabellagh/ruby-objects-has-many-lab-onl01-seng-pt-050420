50f4b3e4f123990ecc4982012e813fbbccdcb13d
isabella g henriques <isabellagh88@gmail.com>
2020-06-01 15:06:25 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,23 @@
-class Post
+class Post 
+  attr_accessor :name, :author 
 
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
+  @@all = []
+
+  def initialize(name, author=nil)
+    @name = name
+    @author = author
+    @@all << self
   end
 
-  def author_name
-    if @author == nil
-      return nil
+  def author
+    if self.author
+      self.author.name
     else
-      @author.name
+      nil
     end
+  end
+
+  def self.all
+    @@all
   end
 end
